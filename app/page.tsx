17f1bee14218fc2f11532d500611f4ea74faa5f2
deviant <mb.deviant@gmail.com>
2025-03-18 08:39:19 +0300

    create initial home screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center justify-items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-      </main>
-    </div>
+    <section className="h-screen flex flex-col justify-center items-center text-center p-8">
+      <h1
+        className="cursor-default font-bold bg-gradient-to-r from-orange-700 via-pink-700 to-purple-700 bg-clip-text text-transparent transition-all duration-300 ease-in-out hover:drop-shadow-[0_0_50px_rgba(255,100,100)]"
+        style={{
+          fontSize: "clamp(3.5rem, 10vw, 8rem)",
+          transform: "scale(1)",
+        }}
+      >
+        mbdeviant
+      </h1>
+      <p className="mt-4 text-sm md:text-lg lg:text-xl text-gray-300">
+        at least that&apos;s the name the internet knows me by
+      </p>
+    </section>
   );
 }
