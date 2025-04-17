848dc570b6b2ef11a511304c63ef049ca89915e1
deviant <mb.deviant@gmail.com>
2025-04-17 20:11:48 +0300

    import about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <div className="snap-y snap-mandatory h-screen">
       <Hero />
+      <About />
     </div>
   );
 }
