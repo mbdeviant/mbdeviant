1e86e3edc443770b1c80b8e02653e3adb687bf0a
deviant <mb.deviant@gmail.com>
2025-04-25 20:22:18 +0300

    update hero component import

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <div className="snap-y snap-mandatory h-screen">
+    <div className="snap-y snap-mandatory h-screen overflow-x-hidden">
       <Hero />
       <About />
     </div>
