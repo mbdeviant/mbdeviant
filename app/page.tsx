c9eb9445fe06e26cd4f038c6ec084b9eece8acb0
deviant <mb.deviant@gmail.com>
2025-05-03 19:49:39 +0300

    import and render projects component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 
 export default function Home() {
   return (
-    <div className="snap-y snap-mandatory h-screen overflow-x-hidden">
+    <div className="snap-y snap-mandatory min-h-screen overflow-x-hidden">
       <Hero />
       <About />
+      <Projects />
     </div>
   );
 }
