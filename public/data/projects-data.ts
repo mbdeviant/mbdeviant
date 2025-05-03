26c3dd4d645af13854545efd4b5a9118540b0b75
deviant <mb.deviant@gmail.com>
2025-05-03 19:50:08 +0300

    create initial project data

---
+++ public/data/projects-data.ts
@@ -0,0 +1,29 @@
+export const projects = [
+  {
+    id: "portfolio",
+    title: "This website",
+    description:
+      "A portfolio website built with Next.js, TypeScript, and Tailwind CSS. It features a 3D hero section with animated waves and a smooth scrolling experience. The website is designed to showcase my work and skills as a developer.",
+    tech: ["Next.js", "TypeScript", "Tailwind CSS"],
+    image: "/data/images/portfolio-preview.png",
+    liveUrl: "https://mbdeviant.com",
+  },
+  {
+    id: "dummy-one",
+    title: "mbdeviant",
+    description:
+      "dummy project data to display in the portfolio. and see how it looks like.",
+    tech: ["Next.js", "TypeScript", "Tailwind CSS"],
+    image: "/data/images/preview.png",
+    liveUrl: "https://x.com",
+  },
+  {
+    id: "dummy-two",
+    title: "mbdeviant",
+    description:
+      "another dummy project data to display in the portfolio. and see how it looks like.",
+    tech: ["Next.js", "TypeScript", "Tailwind CSS"],
+    image: "/data/images/preview2.png",
+    liveUrl: "https://x.com",
+  },
+];
