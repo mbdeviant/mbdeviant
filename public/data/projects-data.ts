a04e0a66601683a8f3917506e8c3c3f6c76ff311
deviant <mb.deviant@gmail.com>
2025-05-03 19:55:49 +0300

    edit image paths

--- public/data/projects-data.ts
+++ public/data/projects-data.ts
@@ -5,7 +5,7 @@ export const projects = [
     description:
       "A portfolio website built with Next.js, TypeScript, and Tailwind CSS. It features a 3D hero section with animated waves and a smooth scrolling experience. The website is designed to showcase my work and skills as a developer.",
     tech: ["Next.js", "TypeScript", "Tailwind CSS"],
-    image: "/data/images/portfolio-preview.png",
+    image: "/data/images/dummy1.jpg",
     liveUrl: "https://mbdeviant.com",
   },
   {
@@ -14,7 +14,7 @@ export const projects = [
     description:
       "dummy project data to display in the portfolio. and see how it looks like.",
     tech: ["Next.js", "TypeScript", "Tailwind CSS"],
-    image: "/data/images/preview.png",
+    image: "/data/images/dummy2.jpg",
     liveUrl: "https://x.com",
   },
   {
@@ -23,7 +23,7 @@ export const projects = [
     description:
       "another dummy project data to display in the portfolio. and see how it looks like.",
     tech: ["Next.js", "TypeScript", "Tailwind CSS"],
-    image: "/data/images/preview2.png",
+    image: "/data/images/dummy3.jpg",
     liveUrl: "https://x.com",
   },
 ];
