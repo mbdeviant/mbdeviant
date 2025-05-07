5d400d8a60272522bb7ae164426f26c61cc8d8c6
deviant <mb.deviant@gmail.com>
2025-05-08 02:43:19 +0300

    import tech stack component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import TechStackSection from "./components/TechStack";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <About />
       <Projects />
+      <TechStackSection />
     </div>
   );
 }
