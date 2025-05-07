6908e5e28e96be4957a98af985882ac8b0658100
deviant <mb.deviant@gmail.com>
2025-05-08 02:42:47 +0300

    add tech data and icons

---
+++ public/data/tech-data.ts
@@ -0,0 +1,27 @@
+export const techStack = {
+  frontend: [
+    { name: "HTML", icon: "/data/icons/frontend/html.svg" },
+    { name: "CSS", icon: "/data/icons/frontend/css.svg" },
+    { name: "Tailwind", icon: "/data/icons/frontend/tailwind.svg" },
+    { name: "JavaScript", icon: "/data/icons/frontend/javascript.svg" },
+    { name: "TypeScript", icon: "/data/icons/frontend/typescript.svg" },
+    { name: "React", icon: "/data/icons/frontend/react.svg" },
+    { name: "Next.js", icon: "/data/icons/frontend/next.svg" },
+    { name: "Framer Motion", icon: "/data/icons/frontend/framer-motion.svg" },
+  ],
+  backend: [
+    { name: "Node.js", icon: "/data/icons/backend/node.svg" },
+    { name: "Express", icon: "/data/icons/backend/express.svg" },
+    { name: "MongoDB", icon: "/data/icons/backend/mongodb.svg" },
+    { name: "PostgreSQL", icon: "/data/icons/backend/postgresql.svg" },
+    { name: "Socket.IO", icon: "/data/icons/backend/socketio.svg" },
+    { name: "Jest", icon: "/data/icons/backend/jest.svg" },
+  ],
+  miscellaneous: [
+    { name: "Git", icon: "/data/icons/misc/git.svg" },
+    { name: "GitHub", icon: "/data/icons/misc/github.svg" },
+    { name: "Webpack", icon: "/data/icons/misc/webpack.svg" },
+    { name: "Vercel", icon: "/data/icons/misc/vercel.svg" },
+    { name: "Linux", icon: "/data/icons/misc/linux.svg" },
+  ],
+};
