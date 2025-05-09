4a2fa34534a782a99eb478799176887a706743bc
deviant <mb.deviant@gmail.com>
2025-05-09 22:55:52 +0300

    edit motion properties

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -17,8 +17,8 @@ export default function Projects() {
             key={project.id}
             initial={{ opacity: 0, x: i % 2 === 0 ? -50 : 50 }}
             whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.6, delay: i * 0.15 }}
-            viewport={{ once: false, amount: 0.4 }}
+            transition={{ duration: 0.2, delay: i * 0.05 }}
+            viewport={{ once: false, amount: 0.1 }}
           >
             <ProjectCard
               project={project}
