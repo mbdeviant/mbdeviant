121af3dac558084bae27cd8f81f949ba7da794bc
deviant <mb.deviant@gmail.com>
2025-05-16 03:36:30 +0300

    change font size

--- app/components/TechStack.tsx
+++ app/components/TechStack.tsx
@@ -18,7 +18,7 @@ const Category = ({
     viewport={{ once: false, amount: 0.6 }}
     className="flex flex-col items-center w-full md:w-1/3"
   >
-    <h3 className="text-2xl font-bold mb-6 text-center text-[#DAA520] tracking-wide">
+    <h3 className="text-4xl  font-medium mb-6 text-center text-[#DAA520] tracking-wide">
       {title}
     </h3>
     <div className="grid grid-cols-3 gap-6 justify-items-center">
