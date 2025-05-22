667272941a0cb9b5d02f2379ab7879415a3008f8
deviant <mb.deviant@gmail.com>
2025-05-22 20:19:38 +0300

    adjust gap and visuals in nav items

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,20 +10,18 @@ export default function Navbar() {
   return (
     <nav className="fixed top-5 left-1/2 -translate-x-1/2 z-15 bg-black/80 backdrop-blur-xs px-6 py-3 rounded-xl shadow-md transition-all duration-300">
       <div className="max-w-7xl mx-auto flex justify-center items-center">
-        <ul className="flex gap-4">
+        <ul className="flex gap-6">
           {sections.map((id) => (
             <li key={id}>
               <a
                 href={`#${id}`}
                 className={`transition-colors duration-200 ${
                   active === id
-                    ? "text-white font-small underline underline-offset-4 decoration-amber-400"
+                    ? "text-amber-400 font-bold"
                     : "text-gray-400 hover:text-white"
                 }`}
               >
-                {id === "hero"
-                  ? "Home"
-                  : id.charAt(0).toUpperCase() + id.slice(1)}
+                {id.charAt(0).toUpperCase() + id.slice(1)}
               </a>
             </li>
           ))}
