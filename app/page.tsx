d79c21b734e7260aad66a8a56f6e9d6c4f824f41
deviant <mb.deviant@gmail.com>
2025-06-14 04:28:48 +0300

    add navbar to index page

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import TechStackSection from "./components/TechStack";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div className="snap-y snap-mandatory min-h-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
       <About />
       <Projects />
