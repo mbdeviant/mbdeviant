193072db7844fa4ce9f543c40fbefd9512c8ad4f
deviant <mb.deviant@gmail.com>
2025-06-18 21:59:19 +0300

    import background canvas in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import BackgroundCanvas from "./components/BackgroundCanvas";
 
 const spaceGrotesk = Space_Grotesk({
   variable: "--font-space-grotesk",
@@ -68,7 +69,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${spaceGrotesk.variable} antialiased`}>{children}</body>
+      <body className={`${spaceGrotesk.variable} antialiased`}>
+        <BackgroundCanvas />
+        {children}
+      </body>
     </html>
   );
 }
