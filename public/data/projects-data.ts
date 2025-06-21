1184dec98fa02b55d9b1633bc3fd6b44225d0cef
deviant <mb.deviant@gmail.com>
2025-06-21 19:59:29 +0300

    change project listing order

--- public/data/projects-data.ts
+++ public/data/projects-data.ts
@@ -15,13 +15,13 @@ export const projects = [
     image: "/data/images/mbdeviant.png",
   },
   {
-    id: "resume-generator",
-    title: "Resume Generator",
+    id: "inventory-app",
+    title: "Inventory App",
     description:
-      "what title says. a resume generator. provide data, save it, download as PDF.",
-    tech: ["JavaScript", "React", "Vite", "react-pdf"],
-    image: "/data/images/resume-generator.png",
-    liveUrl: "https://vite-resume-app.vercel.app/",
+      "add, view and manage products with name, description, category, price, quantity and brand.",
+    tech: ["JavaScript", "Express", "MongoDB", "Pug"],
+    image: "/data/images/inventory-app.png",
+    liveUrl: "https://inventory-app-47ig.onrender.com/home",
   },
   {
     id: "the-things-we-say",
@@ -33,13 +33,13 @@ export const projects = [
     liveUrl: "https://the-things-we-say.onrender.com/",
   },
   {
-    id: "inventory-app",
-    title: "Inventory App",
+    id: "resume-generator",
+    title: "Resume Generator",
     description:
-      "add, view and manage products with name, description, category, price, quantity and brand.",
-    tech: ["JavaScript", "Express", "MongoDB", "Pug"],
-    image: "/data/images/inventory-app.png",
-    liveUrl: "https://inventory-app-47ig.onrender.com/home",
+      "what title says. a resume generator. provide data, save it, download as PDF.",
+    tech: ["JavaScript", "React", "Vite", "react-pdf"],
+    image: "/data/images/resume-generator.png",
+    liveUrl: "https://vite-resume-app.vercel.app/",
   },
   {
     id: "battleship",
